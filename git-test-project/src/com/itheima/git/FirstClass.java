3b4d2b2b3ca895027b01ea46e4637d18e1208d43
husirui <1013504273@qq.com>
2020-08-31 20:41:21 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+import java.sql.SQLOutput;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+    }
+}
